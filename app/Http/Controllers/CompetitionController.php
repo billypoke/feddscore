aa3d22e0230c43af7fd6b150c19a144426ca1885
bacarlso <bacarlso@ncsu.edu>
2016-03-04 10:06:25 -0500

    Add blank controllers

---
+++ app/Http/Controllers/CompetitionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FeddScore\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use FeddScore\Http\Requests;
+use FeddScore\Http\Controllers\Controller;
+
+use FeddScore\Competition;
+
+class CompetitionController extends Controller
+{
+    //
+}
