7881fc469f8f299d0154a48d0c8b1b5c0d72cd23
bacarlso <bacarlso@ncsu.edu>
2016-03-11 08:51:33 -0500

    Add eloquent models

---
+++ app/Competition.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FeddScore;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Competition extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = ['year', 'ampm', 'name', 'status'];
+
+    public function teams()
+    {
+        return $this->hasMany('FeddScore\Team');
+    }
+}
