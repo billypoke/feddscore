d9e8a588e14eeb01d6e4abd5e397f1525f7b8aa3
bacarlso <bacarlso@ncsu.edu>
2016-03-18 09:42:28 -0400

    Add test assertions for competitions and teams
    Green-bar!

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -22,6 +22,13 @@ class IndexTest extends TestCase
         $expectedPhrase = 'Live Scores for Fall 2016';
 
         $this->assertContains($expectedPhrase, $content);
+
+        foreach ($competitions as $competition) {
+            $this->assertContains(htmlspecialchars($competition->name, ENT_QUOTES), $content);
+            foreach ($competition->teams() as $team) {
+                $this->assertContains(htmlspecialchars($team->name, ENT_QUOTES), $content);
+            }
+        }
     }
 
     public function testIndexFinal()
@@ -38,16 +45,36 @@ class IndexTest extends TestCase
         $expected = 'Final Scores for Fall 2016';
 
         $this->assertContains($expected, $content);
+
+        foreach ($competitions as $competition) {
+            $this->assertContains(htmlspecialchars($competition->name, ENT_QUOTES), $content);
+            foreach ($competition->teams() as $team) {
+                $this->assertContains(htmlspecialchars($team->name, ENT_QUOTES), $content);
+            }
+        }
     }
 
     public function testIndexHallOfFame()
     {
+        $competitions = factory(FeddScore\Competition::class, 'final', 3)
+            ->create()
+            ->each(function($competition) {
+                $competition->teams()->save(factory(FeddScore\Team::class)->make());
+            });
+
         $date = \DateTime::createFromFormat('Y-m-d', '2017-01-01');
 
         $content = IndexController::getIndex($date)->render();
         $expected = 'Final Scores for Fall 2016';
 
         $this->assertContains($expected, $content);
+
+        foreach ($competitions as $competition) {
+            $this->assertContains(htmlspecialchars($competition->name, ENT_QUOTES), $content);
+            foreach ($competition->teams() as $team) {
+                $this->assertContains(htmlspecialchars($team->name, ENT_QUOTES), $content);
+            }
+        }
     }
 
     public function testAdvertisement()
