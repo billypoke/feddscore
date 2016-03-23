4d17582d6dc8af9437a2a196ed70df2a54cac9a1
Dustin Wheeler <mdwheele@ncsu.edu>
2016-03-23 11:21:26 -0700

    Move index route into web middleware group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return \FeddScore\Http\Controllers\IndexController::getIndex(new DateTime());
-});
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -27,5 +23,7 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+    Route::get('/', function () {
+        return \FeddScore\Http\Controllers\IndexController::getIndex(new DateTime());
+    });
 });
